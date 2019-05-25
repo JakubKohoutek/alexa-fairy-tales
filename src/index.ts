77d2f46c16b1e362437a765646b08eaa78429036
Jakub Kohoutek <jakub.kohoutek@email.cz>
2019-05-25 18:26:15 +0200

    Add express server to host skill on-premise

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as Alexa from 'ask-sdk';
+import bodyParser from 'body-parser';
+import express from 'express';
 
 import errorHandler from './handlers/error';
 import launchHandler from './handlers/launch';
@@ -7,13 +9,30 @@ import cancelStopIntent from './intents/cancel_stop';
 import helpIntent from './intents/help';
 import playIntent from './intents/play';
 
-export const handler = Alexa.SkillBuilders.custom()
+export const skill = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     launchHandler,
     sessionEndedHandler,
     cancelStopIntent,
     helpIntent,
-    playIntent,
-    errorHandler
+    playIntent
   )
-  .lambda();
+  .addErrorHandlers(errorHandler)
+  .create();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', async (req, res) => {
+  const response = await skill.invoke(req.body);
+  res.send(response);
+});
+
+app.get('/', (req, res) => {
+  res.send('Skill handler is up and running.');
+});
+
+app.listen(3000, () => {
+  // tslint:disable-next-line:no-console
+  console.info('Skill handler is listening on port 3000!');
+});
